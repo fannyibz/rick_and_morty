8a285ea4edee6eb321143369e23c55c6b9e9e814
fannyibanez <fannyibanez89@gmail.com>
2025-02-17 16:11:30 +0100

    Fixing rehydratation error

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -3,19 +3,22 @@ import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/clie
 let client: ApolloClient<NormalizedCacheObject> | null = null;
 
 export function getApolloClient() {
-  if (!client || typeof window === 'undefined') {
+  // Create a new client for server-side rendering
+  if (typeof window === 'undefined') {
+    return new ApolloClient({
+      uri: "https://rickandmortyapi.com/graphql",
+      cache: new InMemoryCache(),
+      ssrMode: true, // Enable SSR mode
+    });
+  }
+
+  // Reuse client on the client-side
+  if (!client) {
     client = new ApolloClient({
       uri: "https://rickandmortyapi.com/graphql",
       cache: new InMemoryCache(),
-      defaultOptions: {
-        query: {
-          fetchPolicy: 'network-only',
-        },
-        watchQuery: {
-          fetchPolicy: 'network-only',
-        },
-      },
     });
   }
+
   return client;
 } 
